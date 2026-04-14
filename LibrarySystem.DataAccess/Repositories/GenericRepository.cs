21f382207df245020e1e0e9c6c7e3e6e589b95f8
MT-div <thaercoding@gmail.com>
2026-04-14 13:29:54 +0300

    Remove updating time from Repository

--- LibrarySystem.DataAccess/Repositories/GenericRepository.cs
+++ LibrarySystem.DataAccess/Repositories/GenericRepository.cs
@@ -1,6 +1,5 @@
 ﻿using LibrarySystem.Application.Interfaces;
 using LibrarySystem.DataAccess.Persistence.Contexts;
-using LibrarySystem.DataAccess.Persistence.models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
@@ -17,72 +16,34 @@ namespace LibrarySystem.DataAccess.Repositories
             _dbSet = context.Set<T>();
         }
 
-        public async Task<T?> GetByIdAsync(int id)
-        {
-            return await _dbSet.FindAsync(id);
-        }
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            // نستخدم شرط IsDeleted إذا كان الكائن يرث من BasePersistenceModel
-            if (typeof(BasePersistenceModel).IsAssignableFrom(typeof(T)))
-            {
-                return await _dbSet.Where(e => !((BasePersistenceModel)(object)e).IsDeleted).ToListAsync();
-            }
+            // لا نحتاج لفحص IsDeleted هنا! 
+            // لأن زميلك وضع Query Filter في ApplicationDbContext
+           
             return await _dbSet.ToListAsync();
         }
-        //params => Eager loading
+
         public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
         {
             IQueryable<T> query = _dbSet;
-
-            // إضافة الـ Includes (مثل جلب الـ Media مع الـ Item)
-            foreach (var include in includes)
-            {
-                query = query.Include(include);
-            }
-
+            foreach (var include in includes) query = query.Include(include);
             return await query.Where(predicate).ToListAsync();
         }
 
-        public async Task AddAsync(T entity)
-        {
-            // تعبئة بيانات التدقيق تلقائياً إذا كان الكائن يرث من BasePersistenceModel
-            if (entity is BasePersistenceModel baseModel)
-            {
-                baseModel.CreatedAt = DateTime.Now;
-                baseModel.IsDeleted = false;
-            }
-            await _dbSet.AddAsync(entity);
-        }
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
 
-        public void Update(T entity)
-        {
-            if (entity is BasePersistenceModel baseModel)
-            {
-                baseModel.ModifiedAt = DateTime.Now;
-            }
-            _dbSet.Update(entity);
-        }
+        public void Update(T entity) => _dbSet.Update(entity);
 
         public void Delete(T entity)
         {
-            // تنفيذ الـ Soft Delete بدلاً من الحذف الفعلي
-            if (entity is BasePersistenceModel baseModel)
-            {
-                baseModel.IsDeleted = true;
-                baseModel.DeletedAt = DateTime.Now;
-                _dbSet.Update(entity);
-            }
-            else
-            {
-                _dbSet.Remove(entity);
-            }
+            // هنا نكتفي بالمسح العادي 
+            // لأن DbContext سيمسك الحالة ويحولها لـ Soft Delete تلقائياً
+            _dbSet.Remove(entity);
         }
 
-        public async Task<int> SaveChangesAsync()
-        {
-            return await _context.SaveChangesAsync();
-        }
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
     }
 }
